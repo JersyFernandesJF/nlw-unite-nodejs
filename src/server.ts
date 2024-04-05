78154f8064f415cb6074d367d75a11d326ecd05e
Jersy Fernandes <102835855+JersyFernandesJF@users.noreply.github.com>
2024-04-05 13:05:49 +0100

    created new table attendee

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,14 @@ import {
   validatorCompiler,
 } from "fastify-type-provider-zod";
 
+import { createEvent } from "./routes/create-event";
+
 const app = fastify();
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
+
+app.register(createEvent)
 
 app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
   console.log("HTTP server running!");
