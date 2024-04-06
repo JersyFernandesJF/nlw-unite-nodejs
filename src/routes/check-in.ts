8be9eea85dd124a2984914e9c069b81f501695cc
Jersy Fernandes <102835855+JersyFernandesJF@users.noreply.github.com>
2024-04-06 07:23:49 +0100

    checkin route

---
+++ src/routes/check-in.ts
@@ -0,0 +1,41 @@
+import { z } from "zod"
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { FastifyInstance } from "fastify";
+
+import { prisma } from "../lib/params";
+
+
+export async function checkIn(app: FastifyInstance)
+{
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .get('',{
+      schema: {
+        params: z.object({
+            attendeeId: z.coerce.number().int()
+        }), 
+        response: {
+          201: z.null
+        }
+      }
+    }, async (request, reply)=>{
+        const { attendeeId } = request.params
+
+        const attendeeCheckIn = await prisma.checkIn.findUnique({
+          where: {
+            attendeeId
+          }
+        })
+        if(attendeeCheckIn !== null){
+          throw new Error('Attendee already checked in!')
+        }
+
+        await prisma.checkIn.create({
+          data:{
+            attendeeId
+          }
+        })
+        return reply.status(201).send()
+      })
+
+}
